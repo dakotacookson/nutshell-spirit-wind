69e3468f9cda02753eb8b4930929ee22ce122ccc
Emmett Schneider <emmett.f.schneider@gmail.com>
2019-01-10 14:31:39 -0600

    messages post to messages API

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 import login from "./login"
+import foodList from "./messages/messagesList"
+import messagesForm from "./messages/messagesForm"
 
 login.createAndAppendLoginInput();
+foodList.fridgify();
+messagesForm.createAndAppendForm();
