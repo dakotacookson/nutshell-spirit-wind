b9536b95cd8943e995df18ec2f67e439f0840d6a
Daniel Brewer <danielbrewer2014@comcast.net>
2019-01-14 10:25:24 -0600

    friends are targeted correctly but username and email are not printinig to DOM yet

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,4 @@
 import login from "./login"
 import registrationForm from "./register";
-import friendsList from "./friends/friendsList";
 login.createAndAppendLoginInput();
 registrationForm.createAndAppendRegistrationForm();
