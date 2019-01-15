53073d12f02447078789287b8990e40a32898ddd
THATBLADEBOY <aus10b@gmail.com>
2019-01-15 11:54:28 -0600

    functioning with master

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,24 @@
+import messagesList from "./messages/messagesList"
+import messagesForm from "./messages/messagesForm"
+import taskForm from "./tasks/taskForm"
+import taskList from "./tasks/taskList"
 import login from "./login"
-import registrationForm from "./register";
-login.createAndAppendLoginInput();
-registrationForm.createAndAppendRegistrationForm();
+import registrationForm from "./register"
+import logout from "./logout"
+import friendsList from "./friends/friendsList"
+
+if (sessionStorage.userId === undefined) {
+  login.createAndAppendLoginInput();
+  registrationForm.createAndAppendRegistrationForm();
+  console.log(sessionStorage.userId)
+} 
+if (sessionStorage.userId >= 1) {
+  logout.createAndAppendLogout();
+  console.log(sessionStorage.userId)
+  messagesList.postMessage();
+  messagesForm.createAndAppendForm();
+  friendsList.getFriendsList()
+  taskForm.taskFormBuilder();
+  taskList.listTasks();
+
+}
